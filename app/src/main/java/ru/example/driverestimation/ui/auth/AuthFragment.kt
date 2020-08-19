3607625588dfa123ac6505718b6f1ff99310a955
Daniil Dobrodeev <danil.dobrodeev@mail.ru>
2020-08-19 12:26:20 +0300

    add auth

--- app/src/main/java/ru/example/driverestimation/ui/auth/AuthFragment.kt
+++ app/src/main/java/ru/example/driverestimation/ui/auth/AuthFragment.kt
@@ -1,11 +1,14 @@
 package ru.example.driverestimation.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fr_auth.*
 import ru.example.driverestimation.R
+import ru.example.driverestimation.ui.personalArea.PersonalAreaActivity
 
 class AuthFragment : Fragment() {
 
@@ -28,5 +31,21 @@ class AuthFragment : Fragment() {
         return inflater.inflate(R.layout.fr_auth, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_sign_in.setOnClickListener {
+            val startPersonalAreaActivity = Intent(activity, PersonalAreaActivity::class.java)
+            startActivity(startPersonalAreaActivity)
+            activity?.finish()
+        }
+
+        btn_sign_up.setOnClickListener {
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.auth_container, RegistrationFragment.newInstance())
+                ?.addToBackStack(RegistrationFragment::class.java.name)
+                ?.commit()
+        }
+    }
 
 }
--- app/src/main/java/ru/example/driverestimation/ui/auth/RegistrationFragment.kt
+++ app/src/main/java/ru/example/driverestimation/ui/auth/RegistrationFragment.kt
@@ -1,13 +1,24 @@
 package ru.example.driverestimation.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fr_auth.*
+import kotlinx.android.synthetic.main.fr_registration.*
+import kotlinx.android.synthetic.main.fr_registration.et_email
 import ru.example.driverestimation.R
+import ru.example.driverestimation.model.User
+import ru.example.driverestimation.utils.SharedPreferencesHelper
+import java.util.*
 
 class RegistrationFragment : Fragment() {
+
+    private val TAG: String = "DEBUG_TAG"
+    var mSharedPreferencesHelper: SharedPreferencesHelper? = null
 
     companion object {
         fun newInstance(): RegistrationFragment {
@@ -26,6 +37,33 @@ class RegistrationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fr_registration, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mSharedPreferencesHelper = SharedPreferencesHelper(activity!!)
+
+        btn_registration_submit.setOnClickListener {
+            if (isDataValid()) {
+                val user = User(
+                    (UUID.randomUUID().toString()).toLong(),
+                    et_email.text.toString(),
+                    et_registration_name.text.toString(),
+                    et_password.text.toString(),
+                    et_registration_car_name.text.toString(),
+                    ""
+                )
+                Log.d(TAG, "onActivityCreated: " + user.toString())
+                //mSharedPreferencesHelper?.addUser(user)
+                fragmentManager?.popBackStack()
+                Toast.makeText(activity, "Successful registration!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isDataValid(): Boolean {
+        return true
     }
 
 }
